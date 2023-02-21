e541d884a2478181dd357c8ed981a94973731fe9
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2023-02-21 04:00:13 +0200

    Created page for user. Use history hook for route to page of user by userId.

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,11 +1,37 @@
 import PropTypes from "prop-types"
+import {useEffect, useState} from "react";
+import api from "../api";
+import QualitiesList from "./QualitiesList";
+import {useHistory} from "react-router-dom";
 
 const UserPage = ({ userId }) => {
-    return(
-        <>
-            <h1>UserPage {userId}</h1>
-        </>
-    )
+    const [user, setUser] = useState()
+
+    const history = useHistory()
+
+    useEffect(() => {
+        api.users.getById(userId).then((data) => setUser(data))
+    }, [])
+
+    if (user) {
+        return(
+            <div>
+                <h1>{user.name}</h1>
+                <h2>Profession: {user.profession.name}</h2>
+                <QualitiesList qualities={user.qualities} />
+                <p>Completed Meetings: {user.completedMeetings}</p>
+                <h2>Rate: {user.rate}</h2>
+                <button type="button"
+                        className="btn btn-primary"
+                        onClick={() => history.replace('/users')}
+                >
+                    All Users
+                </button>
+            </div>
+        )
+    } else {
+        return <h1>Loading...</h1>
+    }
 }
 
 UserPage.propTypes = {
