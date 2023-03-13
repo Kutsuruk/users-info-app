506ba808c0837b5a4b56e6c3ace8153f87fd463e
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2023-03-13 14:07:31 +0200

    Changed configFile, added isFireBase check. Made axios interceptors for request of data from firebase.

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,8 +1,22 @@
 import axios from "axios"
 import { toast } from "react-toastify"
-import config from '../config.json'
+import configFile from '../config.json'
 
-axios.defaults.baseURL = config.apiEndpoint
+axios.defaults.baseURL = configFile.apiEndpoint
+
+axios.interceptors.request.use(
+    function (config){
+        if (configFile.isFireBase) {
+            const containSlash = /\/$/gi.test(config.url)
+            config.url = (containSlash ? config.url.slice(0, -1) : config.url) + '.json'
+        }
+
+        return config
+    },
+    function (error) {
+        return Promise.reject(error)
+    }
+)
 
 axios.interceptors.response.use(
     (res) => res,
